69d9fb84d61323783589983497be0016cd8f83de
l1s22bsse0050 <l1s22bsse0050@ucp.edu.pk>
2025-06-20 15:24:55 +0500

    initial commit

---
+++ setup.sh
@@ -0,0 +1,9 @@
+git init 
+git config --local user.name "l1s22bsse0050"
+git config --local user.email "l1s22bsse0050@ucp.edu.pk"
+git config --local core.editor notepad
+git branch -m develop
+mkdir ./src ./styles ./asssts ./bn ./modules
+touch ./src/index.html ./styles/index.css
+touch .gitignore
+git remote add origin https://github.com/Abran-006/Portfolio-Website.git
